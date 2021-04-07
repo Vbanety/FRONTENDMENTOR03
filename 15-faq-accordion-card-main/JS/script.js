896e809a7da95e595d8184ba33f9bf715cb88507
Vinicius Batista <vbanetyy@gmail.com>
2021-04-07 06:07:10 -0300

    Finish the designer for desktop

---
+++ 15-faq-accordion-card-main/JS/script.js
@@ -0,0 +1,45 @@
+function divShow() {
+    let element = document.getElementById("showText");
+    element.classList.toggle("displayDiv");
+    let bottonDown = document.getElementById("turn-down");
+    bottonDown.classList.toggle("active");
+    let boldText = document.getElementById("bolder");
+    boldText.classList.toggle("bolder-turn");
+  }
+  
+  function divShow2() {
+    let element = document.getElementById("showText2");
+    element.classList.toggle("displayDiv");
+    let bottonDown = document.getElementById("turn-down2");
+    bottonDown.classList.toggle("active");
+    let boldText = document.getElementById("bolder2");
+    boldText.classList.toggle("bolder-turn2");
+  }
+  
+  function divShow3() {
+    let element = document.getElementById("showText3");
+    element.classList.toggle("displayDiv");
+    let bottonDown = document.getElementById("turn-down3");
+    bottonDown.classList.toggle("active");
+    let boldText = document.getElementById("bolder3");
+    boldText.classList.toggle("bolder-turn3");
+  }
+  
+  function divShow4() {
+    let element = document.getElementById("showText4");
+    element.classList.toggle("displayDiv");
+    let bottonDown = document.getElementById("turn-down4");
+    bottonDown.classList.toggle("active");
+    let boldText = document.getElementById("bolder4");
+    boldText.classList.toggle("bolder-turn4");
+  }
+  
+  function divShow5() {
+    let element = document.getElementById("showText5");
+    element.classList.toggle("displayDiv");
+    let bottonDown = document.getElementById("turn-down5");
+    bottonDown.classList.toggle("active");
+    let boldText = document.getElementById("bolder5");
+    boldText.classList.toggle("bolder-turn5");
+  }
+  
