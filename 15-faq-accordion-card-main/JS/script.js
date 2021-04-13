83434f2829aa603a23ba10c96dd80e959cfd8c04
Vinicius Batista <vbanetyy@gmail.com>
2021-04-13 20:13:22 -0300

    Refactoring javascript, inserting media query and changing the classes

--- 15-faq-accordion-card-main/JS/script.js
+++ 15-faq-accordion-card-main/JS/script.js
@@ -1,45 +1,18 @@
-function divShow() {
-    let element = document.getElementById("showText");
-    element.classList.toggle("displayDiv");
-    let bottonDown = document.getElementById("turn-down");
-    bottonDown.classList.toggle("active");
-    let boldText = document.getElementById("bolder");
-    boldText.classList.toggle("bolder-turn");
-  }
-  
-  function divShow2() {
-    let element = document.getElementById("showText2");
-    element.classList.toggle("displayDiv");
-    let bottonDown = document.getElementById("turn-down2");
-    bottonDown.classList.toggle("active");
-    let boldText = document.getElementById("bolder2");
-    boldText.classList.toggle("bolder-turn2");
-  }
-  
-  function divShow3() {
-    let element = document.getElementById("showText3");
-    element.classList.toggle("displayDiv");
-    let bottonDown = document.getElementById("turn-down3");
-    bottonDown.classList.toggle("active");
-    let boldText = document.getElementById("bolder3");
-    boldText.classList.toggle("bolder-turn3");
-  }
-  
-  function divShow4() {
-    let element = document.getElementById("showText4");
-    element.classList.toggle("displayDiv");
-    let bottonDown = document.getElementById("turn-down4");
-    bottonDown.classList.toggle("active");
-    let boldText = document.getElementById("bolder4");
-    boldText.classList.toggle("bolder-turn4");
-  }
-  
-  function divShow5() {
-    let element = document.getElementById("showText5");
-    element.classList.toggle("displayDiv");
-    let bottonDown = document.getElementById("turn-down5");
-    bottonDown.classList.toggle("active");
-    let boldText = document.getElementById("bolder5");
-    boldText.classList.toggle("bolder-turn5");
-  }
-  
+var accItem = document.getElementsByClassName('accordionItem');
+var accHD = document.getElementsByClassName('accordionQuestion');
+
+for (i = 0; i < accHD.length; i++) {
+    accHD[i].addEventListener('click', toggleItem, false);
+}
+
+function toggleItem() {
+    var itemClass = this.parentNode.className;
+    for (i = 0; i < accItem.length; i++) {
+        accItem[i].className = 'accordionItem close';
+    }
+    if (itemClass == 'accordionItem close') {
+        this.parentNode.className = 'accordionItem open';
+    }
+}
+
+
